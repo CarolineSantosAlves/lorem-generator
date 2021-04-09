21aea87aa0776050d872f267fb8258d1ba87fd95
Caroline Santos <caroline.santos.wb@gmail.com>
2021-04-09 11:35:04 -0300

    lorem finalizado

---
+++ public/script.js
@@ -0,0 +1,41 @@
+const myform = document.getElementById('formNum');
+const loremText = document.getElementById('loremText');
+const btnCopiar = document.getElementById('copiar');
+myform.addEventListener('submit', function(e){
+    e.preventDefault();
+    let numParag = document.getElementById('numParagrafo').value;
+    let tipo = document.querySelector('input[name="tipo"]:checked').value;
+
+   
+  
+    fetch('/',{
+        method: 'post',
+        headers: { 'Content-Type': 'application/json'},
+            //'Content-Type': 'application/x-www-form-urlencoded' },
+        //body: formData
+        body: JSON.stringify({
+            numpa : numParag,
+            tipo : tipo
+
+        })
+        
+    }).then(function(response){
+        return response.text(); // se colocar response.json não vai aparecer os \r\n
+    }).then(function(loremGerado){
+        let str = loremGerado.replace(/\\r\\n/gi, '\n');
+      
+        loremText.innerText = str
+    }).catch(function(error){
+        console.log(error)
+    })
+});
+
+ 
+  function copyText() {
+    let elementText = loremText.textContent;
+    navigator.clipboard.writeText(elementText)// retorna um promisse
+    .then(alert('Copiado com sucesso'));  // piscar texto, criar alerta?
+  }
+
+  btnCopiar.addEventListener('click', copyText);
+  
--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 app.use(cors());
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.json()) // habilitando o body parser
+//app.use(express.urlencoded({extended: true}))
 const routes = require('./router');
 
 
-//app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 app.use(routes);
 app.listen(3000, function(){
