e9f48857c65425554b33083ae14de7ed45e8e1be
Caroline Santos <caroline.santos.wb@gmail.com>
2021-04-12 09:56:51 -0300

    ajuste na porta do servidor

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const routes = require('./router');
 app.use(express.static(__dirname + '/public'));
 
 app.use(routes);
-app.listen(3000, function(){
+const port = process.env.PORT || 3000 
+app.listen(port, function(){
     console.log('Servidor rodando na porta 3000');
 })
